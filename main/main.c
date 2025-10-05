f62aff486d304cb4808388ca74e60b807c4e4287
SamcraftSam <71342098+SamcraftSam@users.noreply.github.com>
2025-10-05 05:37:13 +0300

    commit

--- main/main.c
+++ main/main.c
@@ -5,7 +5,10 @@
 #include <freertos/task.h>
 
 #define TIMER_DIVIDER     8
-#define TIMER_INTERVAL    40
+#define TIMER_INTERVAL    20
+
+#define TIMER_FREQ_KHZ  (80 * 1000 * 1000/(TIMER_DIVIDER)/(TIMER_INTERVAL))
+
 #define TIMER_GROUP_ID        TIMER_GROUP_0
 #define TIMER_ID              TIMER_0
 
@@ -16,13 +19,17 @@ volatile uint32_t counter = 0;
 bool IRAM_ATTR onTimer(void *args)
 {
     counter++;
-
-    return false;
+    if (counter > 30*000)
+    {
+        
+        
+    }
+    return true;
 }
 
 void app_main()
 {
-    ESP_LOGI(TAG, "Starting timer example");
+    ESP_LOGI(TAG, "Starting timer example. Freq is: %u", TIMER_FREQ_KHZ);
 
     gpio_set_direction(GPIO_NUM_8, GPIO_MODE_OUTPUT);
     gpio_set_level(GPIO_NUM_8, 1);
@@ -43,8 +50,10 @@ void app_main()
 
     timer_isr_callback_add(TIMER_GROUP_ID, TIMER_ID, onTimer, NULL, 0);
 
+    ESP_LOGI(TAG, "Starting the timer"); 
     timer_start(TIMER_GROUP_ID, TIMER_ID);
 
+    ESP_LOGI(TAG, "Timer was started!");
     while (1) {
         ESP_LOGI(TAG, "Timer count: %lu", counter);
         vTaskDelay(pdMS_TO_TICKS(1000));
